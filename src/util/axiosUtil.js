9bbce4ce85d48fbc88c2baa6563e2db593881617
Revanth Ayodhya <revanthayodhya@gmail.com>
2023-06-11 21:21:29 +0530

    intial commit

---
+++ src/util/axiosUtil.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+import environmentProperties from "../config/environmentProperties";
+
+export async function getAllEmployees() {
+    try {
+        let response = await axios.get(`${environmentProperties.baseUrl}`);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        return [];
+    }
+}
+
+export async function updateEmployeeData(id, body) {
+
+    try {
+        let response = await axios.put(`${environmentProperties.baseUrl}/${id}`, body);
+        if (response.status > 200 && response.status < 300) {
+            return true;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+    return false;
+}
+
+export async function deleteEmployee(id) {
+    try {
+        let response = await axios.delete(`${environmentProperties.baseUrl}/${id}`);
+        if (response.status > 200 && response.status < 300) {
+            return true;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+    return false;
+}
+
+export async function addEmployee(data) {
+    try {
+        let response = await axios.post(`${environmentProperties.baseUrl}/`, data);
+        if (response.status > 200 && response.status < 300) {
+            return true;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+    return false;
+}
